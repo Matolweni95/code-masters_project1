0d51c0ef9387fa1c42f4b14b8697326ed8948ad5
Masixole <masixolematolweni.1@gmail.com>
2023-09-18 09:07:57 +0200

    react-router + tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            
+        },
+
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif']
+        },
+        
+        extend: {},
+    },
+    plugins: [],
+  }
